fedd408f37938b691db96b45e8650f95c86f0024
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2010-11-23 00:51:01 +0000

    have destinations provided for these featurecodes re #4653 (and making paging codes enabled by default)

--- install.php
+++ install.php
@@ -10,21 +10,21 @@ global $amp_conf;
 // Enable intercom as a feature code
 $fcc = new featurecode('paging', 'intercom-prefix');
 $fcc->setDescription('Intercom prefix');
-$fcc->setDefault('*80',false);
+$fcc->setDefault('*80');
 $fcc->update();
 unset($fcc);
 
 // User intercom enable code
 $fcc = new featurecode('paging', 'intercom-on');
 $fcc->setDescription('User Intercom Allow');
-$fcc->setDefault('*54',false);
+$fcc->setDefault('*54');
 $fcc->update();
 unset($fcc);
 
 // User intercom disable 
 $fcc = new featurecode('paging', 'intercom-off');
 $fcc->setDescription('User Intercom Disallow');
-$fcc->setDefault('*55',false);
+$fcc->setDefault('*55');
 $fcc->update();
 unset($fcc);	
 
