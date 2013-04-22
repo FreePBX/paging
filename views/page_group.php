cee4bc75df172fbe10e80694873f1803e0722a11
Moshe Brevda <mbrevda@gmail.com>
2013-04-22 12:15:44 +0000

    Use strict comparison when drawing up the list of selected devices to prevent leading 0's from being misunderstood

--- views/page_group.php
+++ views/page_group.php
@@ -41,7 +41,7 @@ $label = fpbx_label(_('Device List'),
 );
 $selected_dev = $notselected_dev = '';
 foreach ($device_list as $ext => $name) {
-	if (in_array($ext, $devices)) {
+	if (in_array($ext, $devices, true)) {
 		$selected_dev .= '<span data-ext="' . $ext . '">' . $name .'</span>';
 	} else {
 		$notselected_dev .= '<span data-ext="' . $ext . '">' . $name .'</span>';
