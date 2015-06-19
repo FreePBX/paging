9d6e7dd00db2609565697a02f2743907e5b44978
Andrew Nagy <andrew.nagy@the159.com>
2015-06-19 18:21:53 -0400

    [Module Tag script: paging 13.0.2]

--- views/grid.php
+++ views/grid.php
@@ -2,14 +2,16 @@
 
     $dataurl = "ajax.php?module=paging&command=getJSON&jdata=grid";
 ?>
-<a href="config.php?display=paging&view=form" class="btn btn-default"><i class="fa fa-plus"></i>&nbsp; <?php echo _("Add Page Group") ?></a>
- <table id="pagegrid" data-url="<?php echo $dataurl?>" data-cache="false" data-single-select="true" data-checkbox-header="false" data-select-item-name="mkdefault" data-pagination="true" data-search="true" data-toggle="table" class="table table-striped">
+<div id="toolbar-all">
+  <a href="config.php?display=paging&amp;view=form" class="btn btn-default"><i class="fa fa-plus"></i>&nbsp; <?php echo _("Add Page Group") ?></a>
+</div>
+ <table id="pagegrid" data-url="<?php echo $dataurl?>" data-cache="false" data-toolbar="#toolbar-all" data-maintain-selected="true" data-show-columns="true" data-show-toggle="true" data-single-select="true" data-checkbox-header="false" data-select-item-name="mkdefault" data-pagination="true" data-search="true" data-toggle="table" class="table table-striped">
     <thead>
             <tr>
             <th data-field="page_group" data-sortable="true"><?php echo _("Page Group")?></th>
             <th data-field="description"><?php echo _("Description")?></th>
-            <th data-field="page_group" data-formatter="linkFormatter"><?php echo _("Actions")?></th>
             <th data-field="is_default" data-formatter="defaultCheck" data-checkbox="true"><?php echo _("Default")?></th>
+            <th data-field="page_group" data-formatter="linkFormatter"><?php echo _("Actions")?></th>
         </tr>
     </thead>
 </table>
