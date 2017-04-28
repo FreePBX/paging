4dd1ec755afec2a545083b17b1b9ec220c1a3443
Michael Newton <miken32@gmail.com>
2017-04-28 14:36:55 -0700

    Restore usage info (FREEPBX-12690)

--- views/formwrap.php
+++ views/formwrap.php
@@ -7,6 +7,17 @@ if (!defined('FREEPBX_IS_AUTH')) { die('No direct script access allowed'); }
 ?>
 <div class="container-fluid">
 	<h1><?php echo _("Page Group") ?></h1>
+<?php if (!empty($usage_list)):?>
+	<div class="panel panel-default fpbx-usageinfo">
+		<div class="panel-heading">
+			<?php echo $usage_list['text']?>
+		</div>
+		<div class="panel-body">
+			<?php echo $usage_list['tooltip']?>
+		</div>
+	</div>
+
+<?php endif?>
 	<div class = "display full-border">
 		<div class="row">
 			<div class="col-sm-12">
