e549d135237f213cc3e860b848542c4436263aa8
Andrew Nagy <andrew.nagy@the159.com>
2019-04-10 17:51:31 -0400

    update alert infos

--- autoanswerRows.php
+++ autoanswerRows.php
@@ -1,56 +1,56 @@
 <?php
-return [ 
+return [
   [
     'useragent' => 'default',
     'var' => 'ALERTINFO',
     'setting' => 'Ring Answer',
   ],
-   
+
   [
     'useragent' => 'default',
     'var' => 'CALLINFO',
-    'setting' => '\\;answer-after=0',
+    'setting' => '<uri>\\;answer-after=0',
   ],
-   
+
   [
     'useragent' => 'default',
     'var' => 'SIPURI',
     'setting' => 'intercom=true',
   ],
-   
+
   [
     'useragent' => 'Digium',
     'var' => 'ALERTINFO',
     'setting' => 'ring-answer',
   ],
-   
+
   [
     'useragent' => 'Mitel',
     'var' => 'CALLINFO',
-    'setting' => '\\;answer-after=0',
+    'setting' => '<sip:broadworks.net>\\;answer-after=0',
   ],
-   
+
   [
     'useragent' => 'OpenStage',
     'var' => 'ALERTINFO',
-    'setting' => '\\;info=alert-autoanswer',
+    'setting' => '<http://example.com>\\;info=alert-autoanswer',
   ],
-   
+
   [
     'useragent' => 'Panasonic',
     'var' => 'ALERTINFO',
     'setting' => 'Intercom',
   ],
-   
+
   [
     'useragent' => 'Polycom',
     'var' => 'ALERTINFO',
     'setting' => 'info=Auto Answer',
   ],
-   
+
   [
     'useragent' => 'Sangoma',
     'var' => 'ALERTINFO',
-    'setting' => '\\;info=external${PAGE_VOL}',
+    'setting' => '<http://www.sangoma.com>\\;info=external${PAGE_VOL}',
   ],
 ];
