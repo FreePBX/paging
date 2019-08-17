b367a68f87dd7ce3debb6a22d2bc8bdace135fad
nthumann <me@n-thumann.de>
2019-08-17 21:43:34 +0000

    Related to FREEPBX-20414: removed $jobid from runRestore()

--- Restore.php
+++ Restore.php
@@ -2,7 +2,7 @@
 namespace FreePBX\modules\Paging;
 use FreePBX\modules\Backup as Base;
 class Restore Extends Base\RestoreBase{
-	public function runRestore($jobid){
+	public function runRestore(){
 		$configs = $this->getConfigs();
 		foreach ($configs['data'] as $group) {
 			$this->FreePBX->Paging->addGroup($group['page_group'], reset($group['plist']), $group['force_page'], $group['duplex'], $group['description'], $group['is_default'], $group['announcement'], $group['volume']);
